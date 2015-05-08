90a801fca3868f2042f36aed061da4551174742c
Peter Pinch <pdpinch@mit.edu>
2015-05-08 09:02:10 -0400

    standardizing setup.py metadata

--- setup.py
+++ setup.py
@@ -3,14 +3,24 @@ setup.py for PyPi
 """
 from setuptools import setup
 
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
 setup(
     name='xbundle',
     version="0.1.1",
     packages=['xbundle'],
     scripts=['bin/xbundle_convert'],
-    author='Isaac Chuang, Shawn Milochik',
-    author_email='ichuang@mit.edu, milochik@mit.edu',
+    author='MIT ODL Engineering',
+    author_email='odl-engineering@mit.edu',
     description='Converts edX courses between OLX and single-XML formats.',
     url='https://github.com/mitodl/xbundle',
     install_requires=['lxml', 'BeautifulSoup', 'docopt'],
+    license='BSD',
+    long_description=README,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+    ],
 )
