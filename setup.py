f3bb21a4f8cf5a1206b15e11975f272c6e8be54d
Shawn Milochik <shawn@milochik.com>
2015-05-09 20:51:18 -0400

    replacing manual command-line parsing with docopt

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+"""
+setup.py for PyPi
+"""
 from setuptools import setup
 
 setup(
     name='xbundle',
-    version='0.1.0',
+    version="0.1.1",
     packages=['xbundle'],
     scripts=['bin/xbundle_convert'],
     author='Isaac Chuang, Shawn Milochik',
     author_email='ichuang@mit.edu, milochik@mit.edu',
     description='Converts edX courses between OLX and single-XML formats.',
     url='https://github.com/mitodl/xbundle',
-    install_requires=['lxml', 'BeautifulSoup'],
+    install_requires=['lxml', 'BeautifulSoup', 'docopt'],
 )
