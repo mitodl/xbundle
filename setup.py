b6d1db5f1e7f4d008c9e37220b66948679d1b74b
Peter Pinch <pdpinch@mit.edu>
2015-05-14 21:09:22 -0400

    v0.2.0 release notes and increment version [94181746]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='xbundle',
-    version="0.1.1",
+    version="0.2.0",
     packages=['xbundle'],
     scripts=['bin/xbundle_convert'],
     author='MIT ODL Engineering',
